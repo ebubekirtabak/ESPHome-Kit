bee55f94e551485b588b9e2e66f592a4515961ab
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-06-28 14:20:14 +0300

    feat: add heroui

---
+++ client-interface/tailwind.config.js
@@ -0,0 +1,14 @@
+
+const {heroui} = require("@heroui/theme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js"
+],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
