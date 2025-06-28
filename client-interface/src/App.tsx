67f6d5b92dec80de0cbf596bf85967c5bcb09e27
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-06-28 14:33:00 +0300

    feat: init ts to client interface

--- client-interface/src/App.tsx
+++ client-interface/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg';
+import { Button } from '@heroui/button';
 import './App.css'
 
 function App() {
@@ -28,8 +29,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Button color="primary" variant="solid">
+        Button
+      </Button>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ client-interface/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HeroUIProvider } from "@heroui/system";
+import App from './App';
+import './index.css'
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <HeroUIProvider>
+        <App />
+      </HeroUIProvider>
+    </StrictMode>,
+  );
+} else {
+  throw new Error("Root element with id 'root' not found.");
+}
