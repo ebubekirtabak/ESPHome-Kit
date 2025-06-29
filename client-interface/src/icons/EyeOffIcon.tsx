8d3719ba0f4450d22052a6e69f48cbcd5bd1b11c
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-06-29 13:11:19 +0300

    feat: add eye icons

---
+++ client-interface/src/icons/EyeOffIcon.tsx
@@ -0,0 +1,27 @@
+import React, { forwardRef, SVGProps } from 'react';
+
+export type IconProps = SVGProps<SVGSVGElement>;
+
+const EyeOffIcon = forwardRef<SVGSVGElement, IconProps>(
+  function EyeOffIcon({ className = "w-5 h-5 text-gray-400 hover:text-gray-600" }, ref) {
+    return (
+      <svg
+        ref={ref}
+        className={className}
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.878 9.878L3 3m6.878 6.878L21 21"
+        />
+      </svg>
+    );
+  }
+);
+
+
+export default EyeOffIcon;
