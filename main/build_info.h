20955e206c7edbb577ce6a402c422a089c815668
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-07-05 12:43:05 +0300

    feat: add build info

---
+++ main/build_info.h
@@ -0,0 +1,10 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+// Auto-generated build information
+#define BUILD_NUMBER 4
+#define BUILD_DATE __DATE__
+#define BUILD_TIME __TIME__
+#define FIRMWARE_VERSION "1.0.0"
+
+#endif // BUILD_INFO_H
