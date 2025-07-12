4e440cb7ad103c3771119f2969345e9c169ea65f
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-07-12 22:28:35 +0300

    feat: update build number

--- main/build_info.h
+++ main/build_info.h
@@ -2,7 +2,7 @@
 #define BUILD_INFO_H
 
 // Auto-generated build information
-#define BUILD_NUMBER 5
+#define BUILD_NUMBER 7
 #define BUILD_DATE __DATE__
 #define BUILD_TIME __TIME__
 #define FIRMWARE_VERSION "1.0.0"
